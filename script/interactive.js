fef01609d876ac9b102d253cbff9901ab187ab7c
henryheap <henrykaimheap@gmail.com>
2026-03-06 18:23:13 +0000

    Fri, Mar 6, 2026, 10:23 AM -08:00

--- script/interactive.js
+++ script/interactive.js
@@ -2,7 +2,7 @@ interactive = document.querySelector('.interactive')
 bg = document.querySelector('.bg')
 
 
-interactive.addEventListner('click', () => {
+interactive.addEventListener('click', () => {
     bg.style.backgroundImage = "url('../images/burger.avif')"
 
 })
