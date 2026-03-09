eec1fa6f335ca29f29c63875c35550d64e6d1151
henryheap <henrykaimheap@gmail.com>
2026-03-09 16:47:16 +0000

    Mon, Mar 9, 2026, 9:47 AM -07:00

--- script/interactive.js
+++ script/interactive.js
@@ -1,11 +1,15 @@
 clickBox = document.querySelector('.click')
 
 
+colors = ['white', 'green', 'red', 'blue']
+index = 0
+
 clickBox.addEventListener('click', () => {
-    // bg.style.backgroundImage = "url('../images/burger.avif')"
-    // bg.style.backgroundImage = "url('../images/pizza.avif')"
-    // bg.style.backgroundImage = "url('../images/salad.avif')"
-    document.body.style.background = "url('../images/burger.avif')"
-    document.body.style.background = "url('../images/pizza.avif')"
-    document.body.style.background = "url('../images/salad.avif')"
+    clickBox.style.background = colors[index]
+    
+    index = index + 1
+    if (index == colors.length) {
+        index = 0
+    }
+    clickBox.style.color = 'black'
 })
