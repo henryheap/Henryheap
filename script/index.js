f25769f8078eb3bcea79964801d116bea23b38ae
henryheap <henrykaimheap@gmail.com>
2026-03-25 17:14:16 +0000

    Wed, Mar 25, 2026, 10:14 AM -07:00

--- script/index.js
+++ script/index.js
@@ -2,17 +2,6 @@ container = document.querySelector('.container')
 loadText = document.querySelector('.text')
 
 
-load = 0
 
-int = setInterval(blurring, 30)
 
-function blurring() {
-    load = load + 1
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = 1-load/100
-    bg.style.filter = `blur(${50-load/2}px)`
 
-    if (load > 99) {
-        clearInterval(int)
-    }
-}
