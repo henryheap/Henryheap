7d5532b54a53112bb082d125d1671640b288f6e3
henryheap <henrykaimheap@gmail.com>
2026-04-06 09:25:15 -0700

    djfds

--- script/index.js
+++ script/index.js
@@ -1,1 +1,13 @@
+body = document.querySelector('body')
 
+colors = ['white', 'rgb(114,242,148']
+index = 0
+
+body.addEventListener('click', () => {
+    body.style.background = colors[index]
+    index = index + 1
+    if (index == colors.length) {
+        index = 0
+    }
+    body.style.color = 'black'
+})
